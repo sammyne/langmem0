7649a74c378146d2a829c405ce49a59b489edbce
sammyne <xiangmin-li@qq.com>
2026-01-04 05:40:26 +0000

    feat(middleware): impl the async APIs

--- src/langmem0/__init__.py
+++ src/langmem0/__init__.py
@@ -1,3 +1,4 @@
 from langmem0.tools import ManageMemoryTool
+from langmem0.middleware import Mem0Middleware
 
-__all__ = ["ManageMemoryTool"]
+__all__ = ["ManageMemoryTool", "Mem0Middleware"]
--- src/langmem0/middleware.py
+++ src/langmem0/middleware.py
@@ -3,7 +3,8 @@ import logging
 from typing import Awaitable,Any
 
 from langgraph.runtime import Runtime
-from mem0 import Memory
+from mem0 import Memory,AsyncMemory
+from mem0.configs.base import MemoryConfig 
 from langchain.messages import SystemMessage,HumanMessage
 from langchain_openai.chat_models.base import _convert_message_to_dict
 
@@ -21,8 +22,24 @@ class Mem0Middleware(AgentMiddleware):
     def __init__(self, config: dict[str, Any]):
         self.m0 = Memory.from_config(config)
 
-    # def aafter_agent(self, state: AgentState, runtime: Runtime) -> None:
-    #     pass
+        c = AsyncMemory._process_config(config)
+        self.am0 = AsyncMemory(config=MemoryConfig(**c))
+
+    async def aafter_agent(self, state: AgentState, runtime: Runtime) -> None:
+        if not (user_id := extract_user_id(runtime)):
+            return None
+        
+        interaction = [
+            _convert_message_to_dict(v) for v in state["messages"]
+        ]
+
+        logger.debug(f"user-id={user_id}, interaction={interaction}")
+        
+         
+        # https://docs.mem0.ai/integrations/langgraph#create-chatbot-function 
+        # https://docs.mem0.ai/open-source/features/async-memory
+        await self.am0.add(interaction, user_id=user_id)
+        
 
     def after_agent(self, state: AgentState, runtime: Runtime) -> None:
         user_id = extract_user_id(runtime)
@@ -38,12 +55,36 @@ class Mem0Middleware(AgentMiddleware):
         # https://docs.mem0.ai/integrations/langgraph#create-chatbot-function 
         self.m0.add(interaction, user_id=user_id)
 
-    # async def awrap_model_call(
-    #     self,
-    #     request: ModelRequest,
-    #     handler: Callable[[ModelRequest], Awaitable[ModelResponse]],
-    # ) -> ModelCallResult:
-    #     pass
+    async def awrap_model_call(
+        self,
+        request: ModelRequest,
+        handler: Callable[[ModelRequest], Awaitable[ModelResponse]],
+    ) -> ModelCallResult:
+        if not isinstance(request.messages[-1], HumanMessage):
+            return await handler(request)
+
+        if not (user_id := extract_user_id(request.runtime)):
+            return await handler(request)
+        
+        r = await self.am0.search(request.messages[-1].content, user_id=user_id)
+        if not r['results']:
+            return await handler(request)
+
+        # ref: https://docs.langchain.com/oss/python/langchain/middleware/custom#working-with-system-messages
+        # ref: https://docs.mem0.ai/core-concepts/memory-operations/search
+        # ref: https://docs.mem0.ai/integrations/langgraph#create-chatbot-function
+        addon_ctx = "Use the provided context to personalize your responses and remember user preferences and past interactions."
+        for v in r['results']:
+            addon_ctx += f"\n- {v['memory']}"
+
+        logger.debug(f"user-id={user_id}")
+        # logger.debug(f"add-on ctx\n{addon_ctx}")
+        print(f"add-on ctx\n{addon_ctx}")
+        new_content = list(request.system_message.content_blocks) + [
+            {"type": "text", "text": addon_ctx}
+        ]
+        new_system_message = SystemMessage(content=new_content)
+        return await handler(request.override(system_message=new_system_message))
 
     def wrap_model_call(
         self,
