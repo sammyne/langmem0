91653b1fa8ae5b8a03e0818648c561ef6cb31254
sammyne <xiangminli-li@qq.com>
2026-01-06 22:53:00 +0800

    chore: disable linting for some example output

--- examples/chat/helloworld.py
+++ examples/chat/helloworld.py
@@ -84,8 +84,7 @@ r = model.invoke("Suggest dinner options in San Francisco.")
 r.pretty_print()
 
 # Sample output
-# ================================== Ai Message
-# ==================================
+# ================================== Ai Message ==================================  # noqa: E501
 #
 # Since you love Indian food and are allergic to cheese, here are some dinner
 # options for you in San Francisco:
